d4b212273978ce1e6332c6c038274bea768aa413
Miron Kremenetskiy <81591301+miron-kremenetskiy@users.noreply.github.com>
2023-03-10 09:21:40 -0600

    Update question_1.sql

--- question_1.sql
+++ question_1.sql
@@ -1,4 +1,13 @@
-/*What were the number of events, unique sessions, and unique users (i.e., full visitors) that occurred in each hour? By “hour” let's assume we mean the hour in which the event occurred.*/
+with unnest_hits as (
+
+  select
+    md5(cast(ga.fullvisitorid as string) || cast(ga.visitid as string)) as session_id
+    , ga.fullvisitorid
+    , timestamp_seconds(cast(ga.visitStartTime + (hits_unnested.time / 1000) as int64)) as hit_at
+  from `bigquery-public-data.google_analytics_sample.ga_sessions_20170801` as ga
+    , unnest(hits) as hits_unnested
+
+)
 select
   timestamp_trunc(hit_at, hour) as hit_hour
   , count(*) as events
