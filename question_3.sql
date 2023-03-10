6918fd245cea82cc06d0cbdd84e86c0cade1065d
Miron Kremenetskiy <81591301+miron-kremenetskiy@users.noreply.github.com>
2023-03-10 09:25:29 -0600

    Update question_3.sql

--- question_3.sql
+++ question_3.sql
@@ -1,10 +1,20 @@
+with unnest_hits as (
+
+  select
+    md5(cast(ga.fullvisitorid as string) || cast(ga.visitid as string)) as session_id
+    , hits_unnested.eventInfo.eventAction as event_action
+    , ga.totals.transactions as transactions
+  from `bigquery-public-data.google_analytics_sample.ga_sessions_20170801` as ga
+    , unnest(hits) as hits_unnested
+
+)
 select
   case
     when transactions > 0 then 'Purchase'
     else 'Abandoned'
   end as add_to_cart_result
-  , count(distinct session_id) as unique_users
-from unnest_hits
+  , count(distinct session_id) as sessions
+from unnest_hits  
 where event_action = 'Add to Cart'
 group by 1
 ;
